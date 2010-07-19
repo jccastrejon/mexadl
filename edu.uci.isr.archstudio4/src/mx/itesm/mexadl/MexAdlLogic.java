45850e15266d0e94435958e38de47a6d0483c7a3
jccastrejon <jccastrejon@1bbae20c-0c49-396e-e82b-a4c059adaa78>
2010-07-19 17:51:14 +0000

    Initial import.
    git-svn-id: https://mexadl.googlecode.com/svn/trunk@6 1bbae20c-0c49-396e-e82b-a4c059adaa78

---
+++ edu.uci.isr.archstudio4/src/mx/itesm/mexadl/MexAdlLogic.java
@@ -0,0 +1,67 @@
+package mx.itesm.mexadl;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.ui.IWorkbenchActionConstants;
+
+import edu.uci.isr.archstudio4.comp.archipelago.ArchipelagoServices;
+import edu.uci.isr.bna4.AbstractThingLogic;
+import edu.uci.isr.bna4.IBNAMenuListener;
+import edu.uci.isr.bna4.IBNAView;
+import edu.uci.isr.bna4.IThing;
+import edu.uci.isr.xarchflat.ObjRef;
+
+/**
+ * Manage the invocation of the MexADL services.
+ * 
+ * @author jccastrejon
+ * 
+ */
+public class MexAdlLogic extends AbstractThingLogic implements IBNAMenuListener {
+    /**
+     * 
+     */
+    private ObjRef xArchRef;
+
+    /**
+     * 
+     */
+    private ArchipelagoServices archipelagoServices;
+
+    /**
+     * Full constructor.
+     * 
+     * @param archipelagoServices
+     *            ArchipelagoServices.
+     * @param xArchRef
+     *            xADL document reference.
+     */
+    public MexAdlLogic(ArchipelagoServices archipelagoServices, ObjRef xArchRef) {
+        this.archipelagoServices = archipelagoServices;
+        this.xArchRef = xArchRef;
+    }
+
+    @Override
+    public void fillMenu(IBNAView view, IMenuManager menuManager, int localX, int localY, IThing thing, int worldX,
+            int worldY) {
+
+        // This option applies to the whole xADL definition, not to individual
+        // objects
+        if (thing == null) {
+            // adds a menu item
+            menuManager.add(new Action("Generate MexADL artifacts") {
+                @Override
+                public void run() {
+                    MexAdlAnalyzer.analyzeXArch(archipelagoServices.xarch.serialize(xArchRef), ResourcesPlugin
+                            .getWorkspace().getRoot().getRawLocation()
+                            + archipelagoServices.xarch.getXArchURI(xArchRef));
+                }
+            });
+
+            // adds a menu separator
+            menuManager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+        }
+    }
+}
